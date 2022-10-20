7a0b119d09be1f4cbdb0723efe0fa2146c5b575c
unknown <ihzamuttaqin@gmail.com>
2022-10-20 16:56:16 +0800

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewpport" content="width=device=width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    </head>
+    <body>
+        <h3>ini halaman About</h3>
+    </body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+//php artisan make:controller namacontroller (-r) opsional
+
+
+//route about (hyperlink), [(controller yang dituju),(function yang dituju)]
+Route::get('/about', [App\Http\Controllers\aboutcontroller::class, 'index']);
+
+Route::get('/mahasiswa', [App\Http\Controllers\mahasiswacontroller::class, 'index']);
